e074d879c1788e9ad604e70a6ea54a1ab17b2182
Ajas Muhammed N <96200533+Ajasmhdn@users.noreply.github.com>
2026-03-26 23:21:35 +0530

    API POINT CHANGED TO DEPLOYED VERSION

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -60,7 +60,7 @@ export default function Home(): JSX.Element {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:5000/predict",
+        "https://sentioai.hf.space/predict",
         formData,
         {
           headers: {
